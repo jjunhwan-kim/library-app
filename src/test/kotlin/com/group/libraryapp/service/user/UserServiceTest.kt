dccf93a6ff76c8616a357fe66fd821b5f0712f1c
JunHwan Kim <nefertiri972@gmail.com>
2022-09-05 23:08:33 +0900

    Add spring boot test with Junit5
    - Junit5으로 Spring Boot 테스트

---
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -0,0 +1,31 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest @Autowired constructor(
+    private val userRepository: UserRepository,
+    private val userService: UserService
+) {
+
+    @Test
+    fun saveUserTest() {
+
+        // given
+        val request = UserCreateRequest("최태현", null)
+
+        // when
+        userService.saveUser(request)
+
+        // then
+        val results = userRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo(request.name)
+        assertThat(results[0].age).isNull() // 플랫폼 타입
+    }
+}

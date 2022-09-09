b0f9c03f41572f528f022b5ee1eaa18890f6809c
JunHwan Kim <nefertiri972@gmail.com>
2022-09-09 23:58:58 +0900

    Change java book service to kotlin book service
    - BookService.java를 Kotlin으로 변경하고 Optional 제거
    - 확장함수를 사용하여 CrudRepository 인터페이스에 findByIdOrThrow 메서드 추가

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,9 +1,8 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface UserRepository: JpaRepository<User, Long> {
 
-    fun findByName(name: String): Optional<User>
+    fun findByName(name: String): User?
 }

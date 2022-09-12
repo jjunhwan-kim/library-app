6ab12a68fc22c593f2fe660123906d5fc0471e92
JunHwan Kim <nefertiri972@gmail.com>
2022-09-12 23:52:22 +0900

    Add querydsl respository
    - UserLoanHistoryRepository를 Querydsl으로 리팩토링
      - 디폴트 파라미터를 사용한 동적 쿼리 작성
      - Querydsl의 where절에 파라미터가 null일 경우 조건이 동작하지 않고, null이 아닐경우 and조건으로 동작하므로 파라미터에 따라 동적으로 쿼리가 구성됨
      - let scope function을 사용하여 null이 아닐경우에 where 조건이 추가되도록 함
      - fetchOne()은 null이 리턴될 수 있으므로 Elvis 연산자를 사용하여 기본 반환 값 설정

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserLoanHistoryRepository: JpaRepository<UserLoanHistory, Long> {
 
-    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
-
-    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
-
-    fun countByStatus(status: UserLoanStatus): Long
+    //fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
+    //fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
+    //fun countByStatus(status: UserLoanStatus): Long
 
 }
--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -10,6 +10,7 @@ import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.repository.book.BookQuerydslRepository
+import com.group.libraryapp.repository.user.loanhistory.UserLoanHistoryQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,7 +20,8 @@ class BookService(
     private val bookRepository: BookRepository,
     private val bookQuerydslRepository: BookQuerydslRepository,
     private val userRepository: UserRepository,
-    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+    //private val userLoanHistoryRepository: UserLoanHistoryRepository,
+    private val userLoanHistoryQuerydslRepository: UserLoanHistoryQuerydslRepository,
 ) {
 
     @Transactional
@@ -31,7 +33,7 @@ class BookService(
     @Transactional
     fun loanBook(request: BookLoanRequest) {
         val book = bookRepository.findByName(request.bookName) ?: fail()
-        if (userLoanHistoryRepository.findByBookNameAndStatus(request.bookName, UserLoanStatus.LOANED) != null) {
+        if (userLoanHistoryQuerydslRepository.find(request.bookName, UserLoanStatus.LOANED) != null) {
             throw IllegalArgumentException("진작 대출되어 있는 책입니다")
         }
 
@@ -53,7 +55,7 @@ class BookService(
 
         // DB에서 카운트하여 숫자만 받아옴
         // select count(*) from user_loan_history where status = 'LOANED'
-        return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED).toInt()
+        return userLoanHistoryQuerydslRepository.count(UserLoanStatus.LOANED).toInt()
     }
 
     @Transactional(readOnly = true)

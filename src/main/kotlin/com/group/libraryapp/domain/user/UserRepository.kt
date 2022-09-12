078974897978cc68a8f43e66136e50c1183ef1be
JunHwan Kim <nefertiri972@gmail.com>
2022-09-12 23:05:15 +0900

    Add querydsl custom repository
    - Querydsl을 위한 Custom Interface를 정의하고, 해당 인터페이스를 구현한 구현체 작성
    - 기존에 존재하는 Spring Data JPA Repository에서 Querydsl Custom Interface를 상속하여 Spring Data JPA Repository 에서 Querydsl Custom Interface 구현체의 메서드를 사용할 수 있음

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -3,10 +3,10 @@ package com.group.libraryapp.domain.user
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
-interface UserRepository: JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
     fun findByName(name: String): User?
 
-    @Query("select distinct u from User u left join fetch u.userLoanHistories")
-    fun findAllWithHistories(): List<User>
+    //@Query("select distinct u from User u left join fetch u.userLoanHistories")
+    //fun findAllWithHistories(): List<User>
 }

864ce802e931bb88f15a6e718382d7c5f12f0191
JunHwan Kim <nefertiri972@gmail.com>
2022-09-12 23:29:51 +0900

    Add querydsl respository
    - Querydsl 용 Repository 클래스를 작성하여 해당 구현체를 서비스 객체에서 사용하도록 수정
    - 리포지토리에서 엔티티 대신 DTO 바로 반환시 Projections.constructor 사용

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -8,6 +8,6 @@ interface BookRepository : JpaRepository<Book, Long> {
 
     fun findByName(bookName: String): Book?
 
-    @Query("select new com.group.libraryapp.dto.book.response.BookStatResponse(b.type, count(b.id)) from Book b group by b.type")
-    fun getStats(): List<BookStatResponse>
+    //@Query("select new com.group.libraryapp.dto.book.response.BookStatResponse(b.type, count(b.id)) from Book b group by b.type")
+    //fun getStats(): List<BookStatResponse>
 }
--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -9,6 +9,7 @@ import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class BookService(
     private val bookRepository: BookRepository,
+    private val bookQuerydslRepository: BookQuerydslRepository,
     private val userRepository: UserRepository,
     private val userLoanHistoryRepository: UserLoanHistoryRepository,
 ) {
@@ -76,7 +78,7 @@ class BookService(
         */
 
         // DB에서 데이터를 가져올 때 Group By 쿼리를 사용하여 필요한 데이터만 가져옴(인덱스를 사용해 튜닝 할 여지가 있음)
-        return bookRepository.getStats()
+        return bookQuerydslRepository.getStats()
     }
 
 }
